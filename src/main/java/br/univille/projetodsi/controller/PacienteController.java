a6febfa35ada46561a3ed194269eac1f246a03e6
JONATAN HENRIQUE FREUDENBORG <jonatan.freudenborg@univille.br>
2018-06-04 19:39:54 -0300

    first commit

---
+++ src/main/java/br/univille/projetodsi/controller/PacienteController.java
@@ -0,0 +1,25 @@
+package br.univille.projetodsi.controller;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import br.univille.projetodsi.model.Paciente;
+
+@Controller	
+@RequestMapping("/paciente")
+public class PacienteController {
+	@GetMapping("")
+	public ModelAndView index() {
+		List<Paciente> listaPaciente = new ArrayList<Paciente>();
+		Paciente p1 = new Paciente();
+		p1.setNome("Zezinho");
+		p1.setSexo("Masculino");
+		listaPaciente.add(p1);
+		return new ModelAndView("paciente/index", "listapac", listaPaciente);
+	}
+}
