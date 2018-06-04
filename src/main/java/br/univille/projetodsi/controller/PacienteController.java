70bb4bd95b24e53c0805544f093f961074ef9c17
JONATAN HENRIQUE FREUDENBORG <jonatan.freudenborg@univille.br>
2018-06-04 20:38:50 -0300

    Criado interface repositório para classe paciente controller

--- src/main/java/br/univille/projetodsi/controller/PacienteController.java
+++ src/main/java/br/univille/projetodsi/controller/PacienteController.java
@@ -4,22 +4,25 @@ package br.univille.projetodsi.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 import br.univille.projetodsi.model.Paciente;
+import br.univille.projetodsi.repository.PacienteRepository;
 
 @Controller	
 @RequestMapping("/paciente")
 public class PacienteController {
+	
+	@Autowired
+    private PacienteRepository pacienteRepository;
+
 	@GetMapping("")
 	public ModelAndView index() {
-		List<Paciente> listaPaciente = new ArrayList<Paciente>();
-		Paciente p1 = new Paciente();
-		p1.setNome("Zezinho");
-		p1.setSexo("Masculino");
-		listaPaciente.add(p1);
+		List<Paciente> listaPaciente = this.pacienteRepository.findAll();
 		return new ModelAndView("paciente/index", "listapac", listaPaciente);
 	}
 }
