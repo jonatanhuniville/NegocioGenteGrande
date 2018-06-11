057c865d95063a52d282cf68d7031b80e49ce54b
JONATAN HENRIQUE FREUDENBORG <jonatan.freudenborg@univille.br>
2018-06-11 20:41:29 -0300

    sou Foda

--- src/main/java/br/univille/projetodsi/controller/PacienteController.java
+++ src/main/java/br/univille/projetodsi/controller/PacienteController.java
@@ -3,12 +3,17 @@ package br.univille.projetodsi.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.univille.projetodsi.model.Paciente;
 import br.univille.projetodsi.repository.PacienteRepository;
@@ -29,4 +34,10 @@ public class PacienteController {
     public String createForm(@ModelAttribute Paciente paciente) {
         return "paciente/form";
     }
+	
+	@PostMapping(params="form")
+	public ModelAndView save(@Valid Paciente paciente, BindingResult result, RedirectAttributes redirect){
+		paciente = this.pacienteRepository.save(paciente);
+		return new ModelAndView("redirect:/paciente");
+	}
 }
